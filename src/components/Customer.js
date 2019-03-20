41f85ed774f9d21b727da659b160f0875554f9cb
nimoos <sumzzzang26@naver.com>
2019-03-21 01:35:11 +0900

    Material ui

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,43 +1,27 @@
 import React from 'react';
 import { throws } from 'assert';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     render(){ //그려지는 내용이 여기에 render()에 담김
         return(
-            <div>
-            <CustomerProfile image={this.props.image} id={this.props.id} name={this.props.name}/>
-            <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+           
         )
     }
 }
 
 
 
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img scr={this.props.image} alt="profile/"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-    
-}
 
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-
-            </div>
-        )
-    }
-}
 
 export default Customer;
 
